bce9acd8c64e473a5ba1cc92f3b1c2a41b50a47b
jtomfarrar <44488331+jtomfarrar@users.noreply.github.com>
2021-06-03 22:37:27 -0400

    Update ERA5_batch_extraction.py

--- code/ERA5_batch_extraction.py
+++ code/ERA5_batch_extraction.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 """
+
+Work out the extraction of the fields Ray Graham needs for his MS thesis.
+Goal: time series of column-integrated moisture convergence at site
+Requires: either div(q*U) or q*div(U)+grad(q)-dot-U
+Time: 09/01/2016 - 10/31/17
+Lat: 8.5 - 11.5 N.
+Lon: 126.5 - 123.5 W
+
+Modified from:
+C:/Users/jtomf/Documents/Python/WHOTS_WG_proposal/ERA5_batch_extraction.py
+C:/Users/jtomf/Documents/Python/WHOTS_WG_proposal/ERA5_extraction_tool.py
+Also will need to refer to and modify:
+C:/Users/jtomf/Documents/Python/ERA5_plots/read_ERA5_SPURS2_Copernicus_CDS_API.py
+
+
 Created on Thu Jun  3 21:14:11 2021
-Modified from C:/Users/jtomf/Documents/Python/WHOTS_WG_proposal/ERA5_batch_extraction.py
 
 @author: jtomf
 """
